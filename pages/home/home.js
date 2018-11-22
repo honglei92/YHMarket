86ca8886acb2f088211952dc3689d097df293c4e
wanghonglei <836612526@qq.com>
2018-11-22 23:12:52 +0800

    banner  title

--- pages/home/home.js
+++ pages/home/home.js
@@ -5,62 +5,74 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    imgUrls: [
+      'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1542909474745&di=517d07c26bcfa47942d05430ed989c94&imgtype=0&src=http%3A%2F%2Fimg.jc68.com%2F201306%2F04%2F18-11-47-17-1.png',
+      'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1542909584170&di=89a8d0f2e669a208815a0f3dc62b8167&imgtype=0&src=http%3A%2F%2Fpic44.nipic.com%2F20140728%2F7222601_121917173845_2.jpg',
+      'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1542909474745&di=517d07c26bcfa47942d05430ed989c94&imgtype=0&src=http%3A%2F%2Fimg.jc68.com%2F201306%2F04%2F18-11-47-17-1.png',
+      'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1542909584170&di=89a8d0f2e669a208815a0f3dc62b8167&imgtype=0&src=http%3A%2F%2Fpic44.nipic.com%2F20140728%2F7222601_121917173845_2.jpg',
+      'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1542909474745&di=517d07c26bcfa47942d05430ed989c94&imgtype=0&src=http%3A%2F%2Fimg.jc68.com%2F201306%2F04%2F18-11-47-17-1.png',
+      'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1542909584170&di=89a8d0f2e669a208815a0f3dc62b8167&imgtype=0&src=http%3A%2F%2Fpic44.nipic.com%2F20140728%2F7222601_121917173845_2.jpg',
+    ],
+    indicatorDots: true,
+    autoplay: false,
+    interval: 5000,
+    duration: 1000,
+    activityUrl: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1542909071518&di=cfa8ff25da458d2d1221d684160ea736&imgtype=0&src=http%3A%2F%2Fimage.joyj.com%2Fimg%2F2018%2F03%2F14%2Fdbcfd2d3e48d73f00913bceac02d4b00.png',
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
